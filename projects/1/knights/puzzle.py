f1e8677a7fda58fc3d290fcd9fac6eb4d899740b
GeoffreyMesnier <mesniergeoffrey@gmail.com>
2020-05-04 17:12:08 +0200

    Fin projet Knights

--- projects/1/knights/puzzle.py
+++ projects/1/knights/puzzle.py
@@ -12,21 +12,31 @@ CKnave = Symbol("C is a Knave")
 # Puzzle 0
 # A says "I am both a knight and a knave."
 knowledge0 = And(
-    # TODO
+            Or(AKnight,AKnave),                             #Chevalier ou valet
+            Implication(AKnight,And(AKnight,AKnave)),        #Si chevalier-> (AChevalier et AValet)
+            Implication(AKnave,Not(And(AKnight,AKnave)))     #Si Valet -> Not(AChevalier et AValet)
 )
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
 knowledge1 = And(
-    # TODO
+            Or(AKnight,AKnave),                             #A Chevalier ou valet
+            Or(BKnight,BKnave),                             #B Chevalier ou valet
+            Implication(AKnight,And(AKnave,BKnave)),        #Si AChevalier -> (AKnave et BKnave)
+            Implication(AKnave,Not(And(AKnave,BKnave)))      #Si BChevalier -> Not(AValet et BValet)
 )
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
 knowledge2 = And(
-    # TODO
+            Or(AKnight,AKnave),                            #A Chevalier ou valet
+            Or(BKnight,BKnave),                            #B Chevalier ou valet
+            Implication(AKnight,Or(And(AKnight,BKnight),And(AKnave,BKnave))),  #A chevalier -> (Achevalier et BChevalier) ou (AValet et BValet)
+            Implication(BKnight,Or(And(AKnight,BKnave),And(AKnave,BKnight))),
+            Implication(AKnave,Not(Or(And(AKnight,BKnight),And(AKnave,BKnave)))),  #A chevalier -> (Achevalier et BChevalier) ou (AValet et BValet)
+            Implication(BKnave,Not(Or(And(AKnight,BKnave),And(AKnave,BKnight))))   #B chevalier -> (BChevalier et AValet) OU (BValet et Achevalier)
 )
 
 # Puzzle 3
@@ -35,8 +45,17 @@ knowledge2 = And(
 # B says "C is a knave."
 # C says "A is a knight."
 knowledge3 = And(
-    # TODO
+    Or(AKnight,AKnave),                            #A Chevalier ou valet
+    Or(BKnight,BKnave),                            #B Chevalier ou valet
+    Or(CKnight,CKnave),                            #C Chevalier ou valet
+    Implication(BKnight,And(AKnave,CKnave)),       #B chevalier -> AValet et CValet
+    Implication(BKnave,Not(And(AKnave,CKnave))),   #B Valet -> Not (AValet et CValet)
+    Implication(CKnight,AKnight),       #B chevalier -> AValet et CValet
+    Implication(CKnave,AKnave),
+    Implication(AKnight,Or(AKnight,AKnave)),
+    Implication(AKnave,Not(Or(AKnight,AKnave)))
 )
+
 
 
 def main():
